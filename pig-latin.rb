12269b386683907529e984991e4888fac172561d
ks2355244 <ks2355244@yahoo.com>
2016-06-08 10:49:01 +0800

    solved pig latin

--- pig-latin.rb
+++ pig-latin.rb
@@ -1,1 +1,32 @@
 # Place your answer here
+Iteration One
+user_input = word
+word = gets.chomp
+	If
+	 1st capital == (a,e,i,o,u)
+	 puts word
+	else
+	puts word.match{pig latin}
+end
+
+word.match{pig latin} = string
+	string.gsub!(" ")
+	string.gsub!(+/ /"ay")
+end
+
+Print "word.match{pig latin"
+	 
+
+Iteration Two
+
+Puts "Pls type a sentence."
+user_input = gets.chomp
+user_input = sentence.new
+	For
+sentence = word.match{|1st capital| 1st capital (vowel)}
+word.match = string.gsub!(" ")
+end
+
+Print "sentence.new"
+Puts sentence.display {number of word}
+
